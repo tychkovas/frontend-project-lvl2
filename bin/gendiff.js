6c557095c780ad35bd5151f6020627e166e97cf3
tychkovas <tychkovas@yandex.ru>
2020-05-05 02:56:21 +0400

    feature (main) step 1: output help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,15 @@
 #!/usr/bin/env node
-console.log('Hello World!');
+import commander from 'commander';
+
+const { program } = commander;
+
+program
+  .version('1.0.0', '-V, --vers', 'output the current version')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --HELP', 'read more information');
+
+program.parse(process.argv);
+
+if (program.vers) console.log(program.version());
+
+console.log('This is GenDiff.');
