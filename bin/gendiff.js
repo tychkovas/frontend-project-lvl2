30f6c8a5b9f5abdacbe7fce4e2fc029272a93593
tychkovas <tychkovas@yandex.ru>
2020-05-11 11:31:01 +0400

    feature (command) step3: add action to call compare configuration file changes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
 #!/usr/bin/filepath2 node
 import commander from 'commander';
+import { existsSync } from 'fs';
+import genDiff from '../index.js';
 
 const { program } = commander;
 
@@ -9,14 +11,19 @@ program
   .helpOption('-h, --HELP', 'output usage information')
   .option('-f, --format [type]', 'output format');
 
-let file1;
-let file2;
-
 program
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    file1 = filepath1;
-    file2 = filepath2;
+    if (existsSync(filepath1)) {
+      if (existsSync(filepath2)) {
+        const diff = genDiff(filepath1, filepath2);
+        console.log(diff);
+      } else {
+        console.log(`error: file '${filepath2}' does not exists`);
+      }
+    } else {
+      console.log(`error: file '${filepath1}' does not exists`);
+    }
   });
 
 program.parse(process.argv);
@@ -24,7 +31,3 @@ program.parse(process.argv);
 if (program.vers) console.log(program.version());
 
 if (program.format) console.log(` format type: ${program.format}`);
-
-console.log(' filepath1 ', file1, '  filepath2 ', file2);
-
-console.log('This is GenDiff.');
