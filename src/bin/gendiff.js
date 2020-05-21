1b81a30a604c23c78ec651a43947dcfc9a2b1d2f
tychkovas <tychkovas@yandex.ru>
2020-05-21 09:35:52 +0400

    feature (command, bin) step 5: add argument format file, version changed

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,18 +6,24 @@ import genDiff from '../index.js';
 const { program } = commander;
 
 program
-  .version('1.0.0', '-V, --vers', 'output the current version')
+  .version('1.0.1', '-V, --vers', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --HELP', 'output usage information')
   .option('-f, --format [type]', 'output format');
 
+const availableFileFormats = ['json', 'yml'];
+
 program
-  .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
+  .arguments('<filepath1> <filepath2> <fileformat>')
+  .action((filepath1, filepath2, fileformat) => {
     if (existsSync(filepath1)) {
       if (existsSync(filepath2)) {
-        const diff = genDiff(filepath1, filepath2);
-        console.log(diff);
+        if (availableFileFormats.includes(fileformat)) {
+          const diff = genDiff(filepath1, filepath2, fileformat);
+          console.log(diff);
+        } else {
+          console.log(`error: format '${fileformat}' does not available`);
+        }
       } else {
         console.log(`error: file '${filepath2}' does not exists`);
       }
