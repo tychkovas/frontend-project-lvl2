1499b81b34576c4ed8489a3ab495dc1a3b28d2b6
tychkovas <tychkovas@yandex.ru>
2020-05-26 08:43:25 +0400

    test (main) refactor struct test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,43 +6,34 @@ const getFixturesPath = (filename) => join(__dirname, '..', '__fixtures__', file
 
 let pathFile1;
 let pathFile2;
-let pathDiffStylish;
-let fileDiffStylish;
-let diffDataStylish;
-let pathDiffPlain;
-let fileDiffPlain;
-let diffDataPlain;
+
+let pathDiff;
+let fileDiff;
+let diffData;
 
 const pairsFilePath = [
   ['before.json', 'after.json'],
   ['before.yml', 'after.yml'],
   ['before.ini', 'after.ini'],
 ];
+const formats = ['stylish', 'plain'];
+const pathDiffFiles = {
+  stylish: 'diff-stylish.txt',
+  plain: 'diff-plain.txt',
+};
 
-beforeAll(() => {
-  pathDiffStylish = getFixturesPath('diff-stylish.txt');
-  fileDiffStylish = readFileSync(pathDiffStylish, 'UTF-8', 'r');
-  diffDataStylish = fileDiffStylish.split('\n');
-
-  pathDiffPlain = getFixturesPath('diff-plain.txt');
-  fileDiffPlain = readFileSync(pathDiffPlain, 'UTF-8', 'r');
-  diffDataPlain = fileDiffPlain.split('\n');
-});
-
-test.each(pairsFilePath)('test genDiff stylish format file %s from file %s ', (path1, path2) => {
-  pathFile1 = getFixturesPath(path1);
-  pathFile2 = getFixturesPath(path2);
-  const diffResult = genDiff(pathFile1, pathFile2, 'stylish');
-  for (let i = 1; i < diffDataStylish.length; i += 1) {
-    expect(diffResult).toMatch(diffDataStylish[i]);
-  }
-});
-
-test.each(pairsFilePath)('test genDiff plain format file %s from file %s', (path1, path2) => {
-  pathFile1 = getFixturesPath(path1);
-  pathFile2 = getFixturesPath(path2);
-  const diffResult = genDiff(pathFile1, pathFile2, 'plain');
-  for (let i = 1; i < diffDataPlain.length; i += 1) {
-    expect(diffResult).toMatch(diffDataPlain[i]);
-  }
+describe.each(formats)('test dinDiff', (format) => {
+  beforeEach(() => {
+    pathDiff = getFixturesPath(pathDiffFiles[format]);
+    fileDiff = readFileSync(pathDiff, 'UTF-8', 'r');
+    diffData = fileDiff.split('\n');
+  });
+  test.each(pairsFilePath)(`format ${format} for file %s from file %s `, (path1, path2) => {
+    pathFile1 = getFixturesPath(path1);
+    pathFile2 = getFixturesPath(path2);
+    const diffResult = genDiff(pathFile1, pathFile2, format);
+    for (let i = 1; i < diffData.length; i += 1) {
+      expect(diffResult).toMatch(diffData[i]);
+    }
+  });
 });
