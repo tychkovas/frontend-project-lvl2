f0bc0172f8d10e7a3c313108d56634585296e374
tychkovas <tychkovas@yandex.ru>
2020-05-29 10:54:05 +0400

    refactor (format, stylish) changed for new tree

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 
 const stateDiff = ['unmodified', 'add', 'deleted', 'nested', 'modified'];
-const [unmod, add, del, nest, mod] = stateDiff;
+const [unmod, add, del, nested, mod] = stateDiff;
 const tabSize = 4;
 
 const getSign = (curStatus) => {
@@ -9,58 +9,52 @@ const getSign = (curStatus) => {
   return '  '; // unmod && nest
 };
 
-const parseDiffs = (key, data, spaceCnt) => {
+const parseDiffs = (node, spaceCnt) => {
   const getDiff = (obj) => {
-    const [keyObj] = Object.keys(obj);
-    const valueObj = obj[keyObj];
-    const resultObj = parseDiffs(keyObj, valueObj, spaceCnt + tabSize);
+    const resultObj = parseDiffs(obj, spaceCnt + tabSize);
     return resultObj;
   };
-
-  let [state, value] = [null, null];
-  if (Array.isArray(data)) {
-    [state, value] = data;
-  } else {
-    [state, value] = [unmod, data];
-  }
-
-  const lines = [];
+  const key = node.name;
+  const state = node.type;
   const indent = ' '.repeat(spaceCnt);
   const sign = getSign(state);
-  let result;
 
   if (state === mod) {
-    const [valueAdd, valueDel] = value;
-    parseDiffs(key, [add, valueAdd], spaceCnt);
-    parseDiffs(key, [del, valueDel], spaceCnt);
+    // const { nodes } = node;
+    const [valueAdd, valueDel] = node.nodes;
+    const nodeAdd = parseDiffs(valueAdd, spaceCnt);
+    const nodeDel = parseDiffs(valueDel, spaceCnt);
+    return [nodeAdd, nodeDel].flat(3);
   }
-  if (state === mod) {
-    const [valueAdd, valueDel] = value;
-    return [parseDiffs(key, [add, valueAdd], spaceCnt), parseDiffs(key, [del, valueDel], spaceCnt)];
+  const nodeBegin = `${indent} ${sign} ${key}: {`;
+  const nodeEnd = `${indent}    }`;
+
+  if (state === nested) {
+    // const nodeBegin = `${indent} ${sign} ${key}: {`;
+    // const nodeEnd = `${indent}    }`;
+    const { children } = node;
+    const nodeValues = children.map(getDiff);
+    const result = [nodeBegin, nodeValues, nodeEnd].flat(3);
+    return result;
   }
+  // if (state === add)
+  const { value } = node;
   if (typeof value === 'object') {
-    lines.push(`${indent} ${sign} ${key}: {`);
-
-    if (state === nest) {
-      result = value.map(getDiff);
-      lines.push(result);
-    } else {
-      const [value1Key] = Object.keys(value);
-      result = parseDiffs(value1Key, value[value1Key], spaceCnt + tabSize);
-      lines.push(result);
-    }
-
-    lines.push(`${indent}    }`);
-  } else {
-    lines.push(`${indent} ${sign} ${key}: ${value}`);
+    // const nodeBegin = `${indent} ${sign} ${key}: {`;
+    // const nodeEnd = `${indent}    }`;
+    const [vKey] = Object.keys(value);
+    const nodeVal = parseDiffs({ name: vKey, type: unmod, value: value[vKey] }, spaceCnt + tabSize);
+    const resultObj = [nodeBegin, nodeVal, nodeEnd].flat(3);
+    return resultObj;
   }
-  return lines.flat(3);
+  return (`${indent} ${sign} ${key}: ${value}`);
 };
 
 const stylish = (diff) => {
+  console.log('diff: ', diff);
+  console.log(' ---------==============-----------');
   const textDiff = diff.map((obj) => {
-    const [key] = Object.keys(obj);
-    const result = parseDiffs(key, obj[key], 0);
+    const result = parseDiffs(obj, 0);
     return result;
   })
     .flat().join('\n');
@@ -69,3 +63,45 @@ const stylish = (diff) => {
 };
 
 export default stylish;
+
+// const parseDiffs = (node, spaceCnt) => {
+//   const getDiff = (obj) => {
+//     const resultObj = parseDiffs(obj, spaceCnt + tabSize);
+//     return resultObj;
+//   };
+//   const key = node.name;
+//   const state = node.type;
+//   const lines = [];
+//   const indent = ' '.repeat(spaceCnt);
+//   const sign = getSign(state);
+
+//   if (state === mod) {
+//     const { value } = node;
+//     const [valueAdd, valueDel] = value;
+//     const nodeAdd = parseDiffs(valueAdd, spaceCnt);
+//     const nodeDel = parseDiffs(valueDel, spaceCnt);
+//     return [nodeAdd, nodeDel];
+//   }
+
+//   if (state === nested) {
+//     const nodeBegin = `${indent} ${sign} ${key}: {`;
+//     const nodeEnd = (`${indent}    }`;
+//   };
+//   if (typeof value === 'object') {
+//     lines.push(`${indent} ${sign} ${key}: {`);
+
+//     if (state === nested) {
+//       result = value.map(getDiff);
+//       lines.push(result);
+//     } else {
+//       const [value1Key] = Object.keys(value);
+//       result = parseDiffs(value1Key, value[value1Key], spaceCnt + tabSize);
+//       lines.push(result);
+//     }
+
+//     lines.push(`${indent}    }`);
+//   } else {
+//     lines.push(`${indent} ${sign} ${key}: ${value}`);
+//   }
+//   return lines.flat(3);
+// };
