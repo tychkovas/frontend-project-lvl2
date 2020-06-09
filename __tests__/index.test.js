1d9e38a3f47c2868253c9bbac916dc2eed0a9969
tychkovas <tychkovas@yandex.ru>
2020-06-09 12:20:41 +0400

    refactor (test) mentor's comment (removed parsing diffData)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,7 +8,6 @@ let pathFile1;
 let pathFile2;
 
 let pathDiff;
-let fileDiff;
 let diffData;
 
 const pairsFilePath = [
@@ -23,29 +22,15 @@ const pathDiffFiles = {
   json: 'diff-json.json',
 };
 
-describe.each(['stylish', 'plain'])('test dinDiff', (format) => {
+describe.each(['stylish', 'plain', 'json'])('test dinDiff', (format) => {
   beforeEach(() => {
     pathDiff = getFixturesPath(pathDiffFiles[format]);
-    fileDiff = readFileSync(pathDiff, 'UTF-8', 'r');
-    diffData = fileDiff.split('\n');
+    diffData = readFileSync(pathDiff, 'UTF-8');
   });
   test.each(pairsFilePath)(`format ${format} for file %s from file %s `, (path1, path2) => {
     pathFile1 = getFixturesPath(path1);
     pathFile2 = getFixturesPath(path2);
     const diffResult = genDiff(pathFile1, pathFile2, format);
-    for (let i = 1; i < diffData.length; i += 1) {
-      expect(diffResult).toMatch(diffData[i]);
-    }
-  });
-});
-
-describe('test dinDiff fortmat JSON ', () => {
-  const pathDiffJson = getFixturesPath(pathDiffFiles.json);
-  const fileDiffJson = readFileSync(pathDiffJson, 'UTF-8', 'r');
-  test.each(pairsFilePath)('format json for file %s from file %s ', (path1, path2) => {
-    pathFile1 = getFixturesPath(path1);
-    pathFile2 = getFixturesPath(path2);
-    const diffResult = genDiff(pathFile1, pathFile2, 'json');
-    expect(diffResult).toBe(fileDiffJson);
+    expect(diffResult).toBe(diffData);
   });
 });
