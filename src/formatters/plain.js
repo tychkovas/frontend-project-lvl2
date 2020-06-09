c19c188f570e468ee1e06201cf545e2c9168a052
tychkovas <tychkovas@yandex.ru>
2020-06-09 14:35:38 +0400

    refactor (format, plain) add swicth, rename values

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,31 +1,29 @@
 const typesNode = ['unmodified', 'add', 'deleted', 'nested', 'modified'];
 const [unmodified, add, deleted, nested, modified] = typesNode;
-const outValue = (value) => (typeof value !== 'object' ? value : '[complex value]');
+const getPrintValue = (value) => (typeof value !== 'object' ? value : '[complex value]');
 
 const makePlain = (diff) => {
   const getDiffs = (pathObj, obj) => {
     const parseDiffs = (path, node) => {
       const pathPrint = [...path, node.name].join('.');
       const { value } = node;
-      if (node.type === add) {
-        return `Property '${pathPrint}' was added with value: ${outValue(value)}`;
+      switch (node.type) {
+        case add: return `Property '${pathPrint}' was added with value: ${getPrintValue(value)}`;
+        case deleted: return `Property '${pathPrint}' was deleted`;
+        case modified: {
+          const { node: [node1, node2] } = node;
+          return `Property '${pathPrint}' was changed from ${getPrintValue(node1.value)} to ${getPrintValue(node2.value)}`;
+        }
+        case nested: {
+          const { children } = node;
+          return children.map((nestedObj) => getDiffs([...path, node.name], nestedObj))
+            .flat();
+        }
+        case unmodified: return [];
+        default: {
+          throw new Error(`Unknown type of node: '${node.type}'!`);
+        }
       }
-      if (node.type === deleted) {
-        return `Property '${pathPrint}' was deleted`;
-      }
-      if (node.type === modified) {
-        const { node: [node1, node2] } = node;
-        return `Property '${pathPrint}' was changed from ${outValue(node1.value)} to ${outValue(node2.value)}`;
-      }
-      if (node.type === nested) {
-        const { children } = node;
-        return children.map((nestedObj) => getDiffs([...path, node.name], nestedObj))
-          .flat();
-      }
-      if (node.type === unmodified) {
-        return [];
-      }
-      throw new Error(`Unknown type of node: '${node.type}'!`);
     };
     return parseDiffs(pathObj, obj);
   };
@@ -33,7 +31,7 @@ const makePlain = (diff) => {
   const textDiff = diff.map((obj) => getDiffs(rootPath, obj))
     .flat()
     .join('\n');
-  return `${textDiff}`;
+  return textDiff;
 };
 
 export default makePlain;
